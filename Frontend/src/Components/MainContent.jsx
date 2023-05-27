9dab421d45f17d5beb714632598a7c6f40378607
SoeThandarLwin <soethandar.lwin@mail.kmutt.ac.th>
2023-05-27 20:52:46 +0700

    MainContent && css

---
+++ Frontend/src/Components/MainContent.jsx
@@ -0,0 +1,28 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Logo from '../assets/logo.png';
+import '../style/MainContent.css';
+
+function MainContent() {
+    return (
+      <div className="app-container">
+        <div className="app-background-image"/>
+        <img src={Logo} alt="Logo" className="logo" />
+  
+        <div className="logo-buttons-container">
+            <Typography variant="h5" align="center">
+            Are you a   _____?
+            </Typography> <br />
+          <Button variant="contained" color="primary" className="logo-button">
+            Helper
+          </Button>
+  
+          <Button variant="contained" color="primary" className="logo-button">
+            Seeker
+          </Button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default MainContent;
